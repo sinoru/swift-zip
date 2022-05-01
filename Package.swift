96853769a0b66143c10942c9935107d5eff0d005
Jaehong Kang <sinoru@me.com>
2022-05-01 23:56:08 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "swift-zip",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -33,7 +39,7 @@ let package = Package(
     ]
 )
 
-#if swift(>=5.5)
+#if swift(>=5.5) && swift(<5.6)
 package.platforms = [
     .macOS("12"),
     .iOS("15"),
